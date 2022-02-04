f2ae7819a19a90b2290e6a92b7aae3f0ac7ba508
ardagorur <93353754+ardagorur@users.noreply.github.com>
2022-02-04 16:23:54 +0300

    .

---
+++ HomeTechRepair/Models/Identiy/ApplicationRole.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeTechRepair.Models.Identiy
+{
+    public class ApplicationRole:IdentityRole
+    {
+        public ApplicationRole()
+        {
+
+        }
+        public ApplicationRole(string name, String description)
+        {
+            this.Name = name;
+            this.Description = description;
+        }
+        [StringLength(100)]
+        public string Description { get; set; }
+    }
+}
