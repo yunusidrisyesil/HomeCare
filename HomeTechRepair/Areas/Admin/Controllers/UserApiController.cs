9f5d418cf1e2182e1b11e27d475707885fac6be3
edasydmm <75896801+edasydmm@users.noreply.github.com>
2022-02-04 19:59:21 +0300

    Arda

---
+++ HomeTechRepair/Areas/Admin/Controllers/UserApiController.cs
@@ -0,0 +1,24 @@
+﻿using HomeTechRepair.Models.Identiy;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeTechRepair.Areas.Admin.Controllers
+{
+    [Route("api/[controller]/[]action")]
+    [Authorize(Roles = "Admin")]
+    public class UserApiController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HomeTechRepair/Extensions/AppExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeTechRepair.Extensions
+{
+    public static class AppExtensions
+    {
+        public static string ToFullErrorString(this ModelStateDictionary modelState)
+        {
+
+            var message = new List<string>();
+            foreach(var entry in modelState.Values)
+            {
+                foreach (var error in entry.Errors)
+                    message.Add(error.ErrorMessage);
+            }
+
+
+            return string.Join(" ", message);
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
