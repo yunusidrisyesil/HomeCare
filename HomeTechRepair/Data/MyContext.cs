66d091c956952b1aaddff2a05e5b53026af4c981
Yunus İdris Yeşil <yunusyesil85@gmail.com>
2022-02-04 18:43:53 +0300

    DbInit

---
+++ HomeTechRepair/Data/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using HomeTechRepair.Models.Identiy;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeTechRepair.Data
+{
+    public class MyContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+    }
+}
