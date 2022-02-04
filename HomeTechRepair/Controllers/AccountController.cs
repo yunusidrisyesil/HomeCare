4ba98ed94465bd4db6ced9433f0666656518e763
Yunus İdris Yeşil <yunusyesil85@gmail.com>
2022-02-04 20:15:09 +0300

    Register Partially done

--- HomeTechRepair/Controllers/AccountController.cs
+++ HomeTechRepair/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using HomeTechRepair.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HomeTechRepair.Controllers
 {
@@ -10,8 +12,12 @@ namespace HomeTechRepair.Controllers
 
 		private readonly UserManager<ApplicationUser> _userManager;
 
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
-		[HttpGet]
+        [HttpGet]
 		public IActionResult Login()
 		{
 			return View();
@@ -24,15 +30,38 @@ namespace HomeTechRepair.Controllers
 		}
 
 		[HttpPost]
-		public IActionResult Register(RegisterViewModel model)
+		public async Task<IActionResult> Register(RegisterViewModel model)
 		{
             if (!ModelState.IsValid)
             {
 				return View(model);
             }
 
-			var result = _userManager.Users;
+			var isExist = await _userManager.FindByEmailAsync(model.Email);
+			if(isExist == null)
+            {
+				var count = _userManager.Users.Count(); // For username 
+				var user = new ApplicationUser{
+					Name = model.Name,
+					Surname = model.Surname,
+					Email = model.Email,
+					UserName = (count+1).ToString(),
+                };
 
+				var result = await _userManager.CreateAsync(user,model.Password);
+                if (result.Succeeded)
+                {
+					//TODO Email confirmation and user role 
+
+					return RedirectToAction("Login","Account");
+                }
+
+            }
+            else
+            {
+				ModelState.AddModelError(nameof(model.Email),"This email is already in use");
+				return View(model);
+            }
 			return View();
 		}
 	}
