7149d349a726744cb3cda2361d24f137ad18a9a0
ardagorur <93353754+ardagorur@users.noreply.github.com>
2022-02-04 17:48:07 +0300

    PaymentProfile Ok

---
+++ HomeTechRepair/MapperProfiles/PaymentProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HomeTechRepair.Models.Payment;
+using Iyzipay.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeTechRepair.MapperProfiles
+{
+    public class PaymentProfile:Profile
+    {
+        public PaymentProfile()
+        {
+            CreateMap<InstallmentPriceModel, InstallmentPrice>().ReverseMap();
+            CreateMap<InstallmentModel, InstallmentDetail>().ReverseMap();
+            CreateMap<CardModel, PaymentCard>().ReverseMap();
+            CreateMap<BasketModel, BasketItem>().ReverseMap();
+            CreateMap<CustomerModel, Buyer>().ReverseMap();
+            CreateMap<PaymentResponseModel, Payment>().ReverseMap();
+            CreateMap<AddressModel, Address>().ReverseMap();
+        }
+    }
+}
