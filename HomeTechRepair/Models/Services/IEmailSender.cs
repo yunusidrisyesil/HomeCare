974b204e4ef6a7f38fe0d37c5a79c9afa4f3740a
edasydmm <75896801+edasydmm@users.noreply.github.com>
2022-02-04 16:29:20 +0300

    IEmailSender

---
+++ HomeTechRepair/Models/Services/IEmailSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeTechRepair.Models.Services
+{
+    public interface IEmailSender
+    {
+        //Task SendAsync(EmailMessage message);
+
+
+
+    }
+}
