a1cb170ca8336f1c4a243fb99e3e740e5896e266
Yunus İdris Yeşil <yunusyesil85@gmail.com>
2022-02-05 11:29:37 +0300

    Login done

--- HomeTechRepair/Controllers/AccountController.cs
+++ HomeTechRepair/Controllers/AccountController.cs
@@ -7,62 +7,89 @@ using System.Threading.Tasks;
 
 namespace HomeTechRepair.Controllers
 {
-	public class AccountController : Controller
-	{
+    public class AccountController : Controller
+    {
 
-		private readonly UserManager<ApplicationUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
-		public IActionResult Login()
-		{
-			return View();
-		}
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-		[HttpGet]
-		public IActionResult Register()
-		{
-			return View();
-		}
-
-		[HttpPost]
-		public async Task<IActionResult> Register(RegisterViewModel model)
-		{
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
             if (!ModelState.IsValid)
             {
-				return View(model);
+                return View(model);
             }
-
-			var isExist = await _userManager.FindByEmailAsync(model.Email);
-			if(isExist == null)
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null)
             {
-				var count = _userManager.Users.Count(); // For username 
-				var user = new ApplicationUser{
-					Name = model.Name,
-					Surname = model.Surname,
-					Email = model.Email,
-					UserName = (count+1).ToString(),
-                };
-
-				var result = await _userManager.CreateAsync(user,model.Password);
+                var result = await _signInManager.PasswordSignInAsync(user, model.Password, true, false);
                 if (result.Succeeded)
                 {
-					//TODO Email confirmation and user role 
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+            return View();
+        }
 
-					return RedirectToAction("Login","Account");
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var isExist = await _userManager.FindByEmailAsync(model.Email);
+            if (isExist == null)
+            {
+                var count = _userManager.Users.Count(); // For username 
+                var user = new ApplicationUser
+                {
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email,
+                    UserName = (count + 1).ToString(),
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    //TODO Email confirmation and user role 
+
+                    return RedirectToAction("Login", "Account");
                 }
 
             }
             else
             {
-				ModelState.AddModelError(nameof(model.Email),"This email is already in use");
-				return View(model);
+                ModelState.AddModelError(nameof(model.Email), "This email is already in use");
+                return View(model);
             }
-			return View();
-		}
-	}
+            return View();
+        }
+    }
 }
---
+++ HomeTechRepair/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeTechRepair.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string Email { get; set; }
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+    }
+}
