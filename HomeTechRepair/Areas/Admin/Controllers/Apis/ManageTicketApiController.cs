805d40cf7e90af215c207055ec0e3598f8cd99be
edasydmm <75896801+edasydmm@users.noreply.github.com>
2022-02-16 20:04:23 +0300

    Manageticket okeyy :)

--- HomeTechRepair/Areas/Admin/Controllers/Apis/ManageTicketApiController.cs
+++ HomeTechRepair/Areas/Admin/Controllers/Apis/ManageTicketApiController.cs
@@ -38,6 +38,7 @@ namespace HomeTechRepair.Areas.Admin.Controllers.Apis
         [HttpPut]
         public IActionResult Update(Guid key, string values)
         {
+            bool delete = false;
             var data = _dbContext.SupportTickets.Include(x => x.Appointment).Where(x => x.Id == key).FirstOrDefault();
             if (data == null)
                 return BadRequest(new JsonResponseViewModel()
@@ -45,18 +46,43 @@ namespace HomeTechRepair.Areas.Admin.Controllers.Apis
                     IsSuccess = false,
                     ErrorMessage = ModelState.ToFullErrorString()
                 });
+            JsonConvert.PopulateObject(values, data);
             if (data.Appointment == null)
             {
+
                 var appointment = new Appointment()
                 {
                     SupportTicketId = data.Id,
                     AppointmentDate = DateTime.Now
                 };
+                delete = true;
                 _dbContext.Appointments.Add(appointment);
                 _dbContext.SaveChanges();
             }
-            JsonConvert.PopulateObject(values, data);
             JsonConvert.PopulateObject(values, data.Appointment);
+            var date = _dbContext.SupportTickets.Include(x => x.Appointment).Where(x => x.DoctorId == data.DoctorId)
+                .Where(x => x.Appointment.AppointmentDate.Date == data.Appointment.AppointmentDate.Date).ToList();
+            if (date.Count != 0)
+            {
+                foreach (var item in date)
+                {
+                    var eda = _dbContext.Appointments.FirstOrDefault(x => x.SupportTicketId == item.Id);
+                    if (eda.SupportTicketId == data.Id)
+                    {
+                        continue;
+                    }
+                    if (eda.AppointmentDate <= data.Appointment.AppointmentDate && data.Appointment.AppointmentDate <= eda.AppointmentDate.AddHours(1))
+                    {
+                        if (delete)
+                        {
+                            _dbContext.Appointments.Remove(data.Appointment);
+                            _dbContext.SaveChanges();
+                        }
+                        return BadRequest();
+                    }
+                }
+            }
+
             if (!TryValidateModel(data))
                 return BadRequest(ModelState.ToFullErrorString());
             var result = _dbContext.SaveChanges();
