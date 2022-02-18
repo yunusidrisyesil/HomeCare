b89603c8c0071d0ef7f4188e3937fb0768221859
edasydmm <75896801+edasydmm@users.noreply.github.com>
2022-02-18 18:20:43 +0300

    ..

--- HomeTechRepair/Controllers/Apis/ReciptApiController.cs
+++ HomeTechRepair/Controllers/Apis/ReciptApiController.cs
@@ -3,9 +3,11 @@ using HomeTechRepair.Areas.Admin.ViewModels;
 using HomeTechRepair.Data;
 using HomeTechRepair.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace HomeTechRepair.Controllers.Apis
@@ -13,7 +15,7 @@ namespace HomeTechRepair.Controllers.Apis
     [Route("api/[controller]/[action]")]
     public class ReciptApiController : Controller
     {
-       
+
         private readonly MyContext _dbContext;
 
         public ReciptApiController(MyContext dbContext)
@@ -24,17 +26,39 @@ namespace HomeTechRepair.Controllers.Apis
         [HttpGet]
         public IActionResult Get(DataSourceLoadOptions loadOptions)
         {
-            var data = _dbContext.ReciptMasters.Where(x => x.UserId == HttpContext.GetUserId()).Select(x => new ReciptViewModel
-            {
+            var data = _dbContext.ReciptMasters
+                .Where(x => x.UserId == "c2b8f513-32ed-470b-aa9e-57788439b6aa")
+                .Select(x => new ReciptViewModel
+                {
 
-                Id = x.Id,
-                TotalAmount = x.TotalAmount,
-                Date = x.Date,
-                UserId = x.UserId
-            }).ToList();
-
+                    ReciptMasterId = x.Id,
+                    TotalAmount = x.TotalAmount,
+                    Date = x.Date,
+                    UserId = x.UserId,
+                    hasitems = _dbContext.ReciptDetails.Count(b => b.ReciptMasterId == x.Id) > 0
+                }).ToList();
             return Ok(DataSourceLoader.Load(data, loadOptions));
+        }
+
+        [HttpGet]
+        public IActionResult GetReciptDetail(DataSourceLoadOptions loadOptions)
+        {
+            var datard = _dbContext.ReciptDetails.Include(x => x.ReciptMaster)
+              .Include(x => x.Service).Select(x => new ReciptViewModel
+              {
+                  ReciptMasterId = x.ReciptMasterId,
+                  Id = x.ServiceId,
+                  Name = x.Service.Name,
+                  ServicePrice = x.ServicePrice,
+                  Quantity = x.Quantity,
+                  Description = x.Description
+              }).ToList();
+
+
+            return Ok(DataSourceLoader.Load(datard, loadOptions));
+
         }
 
     }
 }
+
