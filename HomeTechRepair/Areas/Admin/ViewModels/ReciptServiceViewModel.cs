af4e5f719183f08487fba93b523d6d697b12eaa3
Yunus İdris Yeşil <yunusyesil85@gmail.com>
2022-02-18 17:42:47 +0300

    CloseTicketApiController done

---
+++ HomeTechRepair/Areas/Admin/ViewModels/ReciptServiceViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeTechRepair.Areas.Admin.ViewModels
+{
+    public class ReciptServiceViewModel
+    {
+        public Guid Id { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
